5a68b0c75fc337c452dbf4999d7e7f6752f80e37
Scala Steward <me@scala-steward.org>
2020-01-05 20:28:02 +0100

    Update cats-scalacheck to 0.2.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val ScapegoatVersion      = "1.3.8"
   val ShapelessVersion      = "2.3.3"
   val spdxCheckerVersion    = "1.0.0"
-  val scalacheckCatsVersion = "0.1.1"
+  val scalacheckCatsVersion = "0.2.0"
   val scalatestVersion      = "3.0.4"
   val scalacheckVersion     = "1.14.0"
   val jts                   = "1.16.1"
