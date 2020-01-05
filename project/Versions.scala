7be72d14efbcaf58588295b35c83d609073cfbb7
Scala Steward <me@scala-steward.org>
2020-01-05 20:28:17 +0100

    Update circe-core, circe-generic, ... to 0.12.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val CatsVersion           = "2.0.0"
-  val CirceVersion          = "0.12.2"
+  val CirceVersion          = "0.12.3"
   val GeoTrellisVersion     = "3.2.0"
   val RefinedVersion        = "0.9.10"
   val ScapegoatVersion      = "1.3.8"
