eeeae8f500a2bbf34af520850dff592d0f3fa9aa
Scala Steward <me@scala-steward.org>
2020-02-21 08:29:05 +0100

    Update scalatest to 3.1.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val ShapelessVersion        = "2.3.3"
   val spdxCheckerVersion      = "1.0.0"
   val scalacheckCatsVersion   = "0.2.0"
-  val scalatestVersion        = "3.1.0"
+  val scalatestVersion        = "3.1.1"
   val scalacheckVersion       = "1.14.3"
   val ScalatestPlusScalacheck = "3.1.0.1"
   val jts                     = "1.16.1"
