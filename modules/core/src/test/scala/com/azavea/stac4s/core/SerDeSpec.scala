e6a3905d5a1d992c59a936c50e24589482745d0c
James Santucci <james.santucci@gmail.com>
2020-03-16 15:08:29 -0500

    wip -- use unserializableCodec, which was like 10 lines away :man_facepalming:

--- modules/core/src/test/scala/com/azavea/stac4s/core/SerDeSpec.scala
+++ modules/core/src/test/scala/com/azavea/stac4s/core/SerDeSpec.scala
@@ -3,7 +3,7 @@ package com.azavea.stac4s
 import com.azavea.stac4s.meta._
 import com.azavea.stac4s.Generators._
 
-// import geotrellis.vector.Geometry
+import geotrellis.vector.Geometry
 import io.circe.testing.{ArbitraryInstances, CodecTests}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.Checkers
@@ -13,33 +13,33 @@ import java.time.Instant
 
 class SerDeSpec extends AnyFunSuite with FunSuiteDiscipline with Checkers with ArbitraryInstances {
 
-  checkAll("Codec.StacMediaType", CodecTests[StacMediaType].codec)
-  checkAll("Codec.StacAssetRole", CodecTests[StacAssetRole].codec)
-  checkAll("Codec.StacLinkType", CodecTests[StacLinkType].codec)
-  checkAll("Codec.StacProviderRole", CodecTests[StacProviderRole].codec)
+  checkAll("Codec.StacMediaType", CodecTests[StacMediaType].unserializableCodec)
+  checkAll("Codec.StacAssetRole", CodecTests[StacAssetRole].unserializableCodec)
+  checkAll("Codec.StacLinkType", CodecTests[StacLinkType].unserializableCodec)
+  checkAll("Codec.StacProviderRole", CodecTests[StacProviderRole].unserializableCodec)
 
-  checkAll("Codec.Instant", CodecTests[Instant].codec)
-  // checkAll("Codec.Geometry", CodecTests[Geometry].codec)
+  checkAll("Codec.Instant", CodecTests[Instant].unserializableCodec)
+  checkAll("Codec.Geometry", CodecTests[Geometry].unserializableCodec)
 
-  checkAll("Codec.StacItemAsset", CodecTests[StacItemAsset].codec)
-  checkAll("Codec.StacCollectionAsset", CodecTests[StacCollectionAsset].codec)
+  checkAll("Codec.StacItemAsset", CodecTests[StacItemAsset].unserializableCodec)
+  checkAll("Codec.StacCollectionAsset", CodecTests[StacCollectionAsset].unserializableCodec)
 
-  checkAll("Codec.SPDX", CodecTests[SPDX].codec)
+  checkAll("Codec.SPDX", CodecTests[SPDX].unserializableCodec)
 
-  checkAll("Codec.StacItem", CodecTests[StacItem].codec)
+  checkAll("Codec.StacItem", CodecTests[StacItem].unserializableCodec)
 
-  checkAll("Codec.ItemCollection", CodecTests[ItemCollection].codec)
+  checkAll("Codec.ItemCollection", CodecTests[ItemCollection].unserializableCodec)
 
-  checkAll("Codec.StacCatalog", CodecTests[StacCatalog].codec)
+  checkAll("Codec.StacCatalog", CodecTests[StacCatalog].unserializableCodec)
 
-  checkAll("Codec.TwoDimBbox", CodecTests[TwoDimBbox].codec)
+  checkAll("Codec.TwoDimBbox", CodecTests[TwoDimBbox].unserializableCodec)
 
-  checkAll("Codec.ThreeDimBbox", CodecTests[ThreeDimBbox].codec)
+  checkAll("Codec.ThreeDimBbox", CodecTests[ThreeDimBbox].unserializableCodec)
 
-  checkAll("Codec.TemporalExtent", CodecTests[TemporalExtent].codec)
-  checkAll("Codec.Bbox", CodecTests[Bbox].codec)
-  checkAll("Codec.StacExtent", CodecTests[StacExtent].codec)
+  checkAll("Codec.TemporalExtent", CodecTests[TemporalExtent].unserializableCodec)
+  checkAll("Codec.Bbox", CodecTests[Bbox].unserializableCodec)
+  checkAll("Codec.StacExtent", CodecTests[StacExtent].unserializableCodec)
 
-  checkAll("Codec.StacCollection", CodecTests[StacCollection].codec)
+  checkAll("Codec.StacCollection", CodecTests[StacCollection].unserializableCodec)
 
 }
