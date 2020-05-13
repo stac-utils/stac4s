c160899de129439082ddee5ea33c64a95f5cb48b
Grigory Pomadchin <gr.pomadchin@gmail.com>
2020-05-13 15:29:03 -0400

    Extension properties should follow STAC SPEC

--- modules/core/src/main/scala/com/azavea/stac4s/extensions/layer/LayerProperties.scala
+++ modules/core/src/main/scala/com/azavea/stac4s/extensions/layer/LayerProperties.scala
@@ -4,19 +4,19 @@ import cats.Eq
 import io.circe.{Decoder, Encoder}
 import io.circe.syntax._
 
-case class LayerProperties(layers: List[String])
+case class LayerProperties(ids: List[String])
 
 object LayerProperties {
   implicit val eqLayerProperties: Eq[LayerProperties] = Eq.fromUniversalEquals
 
   implicit val encLayerProperties: Encoder.AsObject[LayerProperties] = Encoder.AsObject.instance[LayerProperties] { o =>
-    Map("layers" -> o.layers.asJson).asJsonObject
+    Map("layer:ids" -> o.ids.asJson).asJsonObject
   }
 
   implicit val decLayerProperties: Decoder[LayerProperties] = Decoder[Map[String, List[String]]] emap {
-    _.get("layers") match {
+    _.get("layer:ids") match {
       case Some(l) => Right(LayerProperties(l))
-      case _       => Left("Could not decode LayerPropertirs")
+      case _       => Left("Could not decode LayerProperties.")
     }
   }
 }
