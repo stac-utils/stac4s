0f0427f36bbf5d52e3181ef44fab4c21696c9430
James Santucci <james.santucci@gmail.com>
2020-05-26 13:41:54 -0500

    Allow band nums to be any positive number

--- modules/core/src/main/scala/com/azavea/stac4s/extensions/eo/package.scala
+++ modules/core/src/main/scala/com/azavea/stac4s/extensions/eo/package.scala
@@ -3,11 +3,11 @@ package com.azavea.stac4s.extensions
 import eu.timepit.refined._
 import eu.timepit.refined.api._
 import eu.timepit.refined.numeric._
+import eu.timepit.refined.types.numeric.PosInt
 
 package object eo {
   type Percentage = Int Refined Interval.Closed[W.`0`.T, W.`100`.T]
   object Percentage extends RefinedTypeOps[Percentage, Int]
 
-  type BandRange = Int Refined Interval.Closed[W.`0`.T, W.`100`.T]
-  object BandRange extends RefinedTypeOps[BandRange, Int]
+  type BandRange = PosInt
 }
