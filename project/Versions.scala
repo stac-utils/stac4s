f403e8a43f96c173c8d4a5e3f1ad32bcba25004c
Scala Steward <me@scala-steward.org>
2020-09-20 10:07:21 +0200

    Update refined, refined-scalacheck to 0.9.16

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val CatsVersion             = "2.2.0"
   val CirceVersion            = "0.13.0"
   val GeoTrellisVersion       = "3.5.0"
-  val RefinedVersion          = "0.9.15"
+  val RefinedVersion          = "0.9.16"
   val ScapegoatVersion        = "1.3.11"
   val ShapelessVersion        = "2.3.3"
   val spdxCheckerVersion      = "1.0.0"
