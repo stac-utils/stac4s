7e3acc0fe659350239051400d402e0afd07b1ca8
Scala Steward <me@scala-steward.org>
2020-09-29 18:08:19 +0200

    Update jts-core to 1.17.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val CirceVersion            = "0.13.0"
   val GeoTrellisVersion       = "3.5.0"
   val JodaTime                = "2.10.6"
-  val Jts                     = "1.16.1"
+  val Jts                     = "1.17.1"
   val RefinedVersion          = "0.9.17"
   val ScalacheckCatsVersion   = "0.3.0"
   val ScalacheckVersion       = "1.14.3"
