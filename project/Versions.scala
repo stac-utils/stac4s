44c469a378f0c6a8c0b5423e1916d392bf8d61dc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-31 18:13:51 +0100

    Update scalacheck to 1.15.0 (#161)

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val Jts                     = "1.16.1"
   val RefinedVersion          = "0.9.17"
   val ScalacheckCatsVersion   = "0.3.0"
-  val ScalacheckVersion       = "1.14.3"
+  val ScalacheckVersion       = "1.15.0"
   val ScalatestPlusScalacheck = "3.2.2.0"
   val ScalatestVersion        = "3.2.2"
   val ScapegoatVersion        = "1.3.11"
