52bc11a5eceba422ffc7cfe040988732308ef342
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-09 08:21:36 +0100

    Update scalatest to 3.2.3 (#170)

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val ScalacheckCatsVersion   = "0.3.0"
   val ScalacheckVersion       = "1.15.1"
   val ScalatestPlusScalacheck = "3.2.2.0"
-  val ScalatestVersion        = "3.2.2"
+  val ScalatestVersion        = "3.2.3"
   val ScapegoatVersion        = "1.3.11"
   val ShapelessVersion        = "2.3.3"
   val SpdxCheckerVersion      = "1.0.0"
