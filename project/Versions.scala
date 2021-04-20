edaa037db34e1a5237a5f56c7aec15553a3d0bf4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-04-21 00:31:41 +0200

    Update scalatest to 3.2.8 (#291)

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val ScalacheckCats          = "0.3.0"
   val Scalacheck              = "1.15.3"
   val ScalatestPlusScalacheck = "3.2.2.0"
-  val Scalatest               = "3.2.7"
+  val Scalatest               = "3.2.8"
   val Scapegoat               = "1.4.8"
   val Shapeless               = "2.3.4"
   val SpdxChecker             = "1.0.0"
