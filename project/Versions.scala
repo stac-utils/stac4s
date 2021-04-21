90f89512678fd20350ea3e649e8d3d9f5ec87557
Scala Steward <me@scala-steward.org>
2021-04-21 14:16:13 +0200

    Update refined, refined-scalacheck to 0.9.24

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val Enumeratum              = "1.6.1"
   val GeoTrellis              = "3.5.2"
   val Jts                     = "1.16.1"
-  val Refined                 = "0.9.23"
+  val Refined                 = "0.9.24"
   val ScalacheckCats          = "0.3.0"
   val Scalacheck              = "1.15.3"
   val ScalatestPlusScalacheck = "3.2.2.0"
