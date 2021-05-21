e85ee2fd9cfc3d878530f8093f5951c47de66919
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-05-21 10:54:20 -0400

    Rename type alias

--- modules/client/js/src/main/scala/com/azavea/stac4s/api/client/package.scala
+++ modules/client/js/src/main/scala/com/azavea/stac4s/api/client/package.scala
@@ -3,6 +3,6 @@ package com.azavea.stac4s.api
 package object client {
   type SttpStacClient[F[_]]            = SttpStacClientF[F, SearchFilters]
   type StacClient[F[_]]                = StacClientF[F, SearchFilters]
-  type StreamingClient[F[_]]           = StreamingStacClientF[F, fs2.Stream[F, *], SearchFilters]
+  type StreamingStacClientFS2[F[_]]    = StreamingStacClientF[F, fs2.Stream[F, *], SearchFilters]
   type StreamingStacClient[F[_], G[_]] = StreamingStacClientF[F, G, SearchFilters]
 }
