5bf64d6c03909f74c1c4092afc6d64ed8a25efc3
James Santucci <james.santucci@gmail.com>
2021-05-28 13:26:39 -0600

    fix generated stac versions

--- modules/testing/jvm/src/main/scala/JvmInstances.scala
+++ modules/testing/jvm/src/main/scala/JvmInstances.scala
@@ -202,7 +202,7 @@ trait JvmInstances extends GenericInstances {
   private[testing] def itemCollectionGen: Gen[ItemCollection] =
     (
       Gen.const("FeatureCollection"),
-      Gen.const(StacVersion.unsafeFrom("1.0.0-rc2")),
+      Gen.const(StacVersion.unsafeFrom("1.0.0")),
       Gen.const(Nil),
       Gen.listOf[StacItem](stacItemGen),
       Gen.listOf[StacLink](TestInstances.stacLinkGen),
@@ -212,7 +212,7 @@ trait JvmInstances extends GenericInstances {
   private[testing] def itemCollectionShortGen: Gen[ItemCollection] =
     (
       Gen.const("FeatureCollection"),
-      Gen.const(StacVersion.unsafeFrom("1.0.0-rc2")),
+      Gen.const(StacVersion.unsafeFrom("1.0.0")),
       Gen.const(Nil),
       Gen.listOf[StacItem](stacItemGen),
       Gen.const(Nil),
