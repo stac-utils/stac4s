5b70c1851aa0bdfe0a6cf5e47d9e4b9b9ed7f42f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-09 02:13:06 +0200

    Update fs2-core to 2.5.8 (#362)

--- project/Versions.scala
+++ project/Versions.scala
@@ -29,6 +29,6 @@ object Versions {
   val Sttp                    = "3.3.9"
   val SttpModel               = "1.4.7"
   val SttpShared              = "1.2.5"
-  val Fs2                     = "2.5.7"
+  val Fs2                     = "2.5.8"
   val ThreeTenExtra           = "1.6.0"
 }
