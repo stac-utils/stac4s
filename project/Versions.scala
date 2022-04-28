386d12cee72452b9c89a38f62cfcf83a4c03193d
Scala Steward <me@scala-steward.org>
2022-04-28 21:09:30 +0200

    Update shared:core to 1.3.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,7 +28,7 @@ object Versions {
   val Shapeless               = "2.3.7"
   val Sttp                    = "3.5.2"
   val SttpModel               = "1.4.25"
-  val SttpShared              = "1.3.4"
+  val SttpShared              = "1.3.5"
   val Fs2                     = "2.5.10"
   val ThreeTenExtra           = "1.7.0"
 }
