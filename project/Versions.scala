82ba6224797caaafcc617ca9a100eaff49903767
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-29 14:18:34 +0200

    Update model:core to 1.4.26 (#522)

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,7 +27,7 @@ object Versions {
   val Scapegoat               = "1.4.12"
   val Shapeless               = "2.3.7"
   val Sttp                    = "3.5.2"
-  val SttpModel               = "1.4.25"
+  val SttpModel               = "1.4.26"
   val SttpShared              = "1.3.4"
   val Fs2                     = "2.5.11"
   val ThreeTenExtra           = "1.7.0"
