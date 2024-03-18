8cb5f91432f3d6c37231a3569f58180423f93757
Scala Steward <scala_steward@virtuslab.com>
2024-03-18 21:18:21 +0000

    Update model:core to 1.7.9

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,7 +27,7 @@ object Versions {
   val Scapegoat               = "2.1.5"
   val Shapeless               = "2.3.10"
   val Sttp                    = "3.9.4"
-  val SttpModel               = "1.7.7"
+  val SttpModel               = "1.7.9"
   val SttpShared              = "1.3.17"
   val Fs2                     = "3.9.4"
   val ThreeTenExtra           = "1.7.2"
