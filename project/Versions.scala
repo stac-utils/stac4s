ca01e57e65932b70bd485b413b75af9bc2ebe69b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-07-12 03:43:16 +0200

    Update enumeratum, enumeratum-circe, ... to 1.7.4 (#684)

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val Circe                   = "0.14.9"
   val CirceJsonSchema         = "0.2.0"
   val DisciplineScalatest     = "2.3.0"
-  val Enumeratum              = "1.7.3"
+  val Enumeratum              = "1.7.4"
   val GeoTrellis              = "3.7.1"
   val Jts                     = "1.19.0"
   val Monocle                 = "2.1.0"
