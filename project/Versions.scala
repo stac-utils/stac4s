47dc72426933be6347551ab4e2fb75c639520182
Scala Steward <scala_steward@virtuslab.com>
2024-10-16 23:10:05 +0000

    Update client3:circe, client3:core, ... to 3.10.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,7 +27,7 @@ object Versions {
   val Scalatest               = "3.2.19"
   val Scapegoat               = "3.0.3"
   val Shapeless               = "2.3.12"
-  val Sttp                    = "3.10.0"
+  val Sttp                    = "3.10.1"
   val SttpModel               = "1.7.11"
   val SttpShared              = "1.3.22"
   val Fs2                     = "3.11.0"
