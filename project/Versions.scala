615400eeaa19c2321151fd2b5933aa800927c5c6
Scala Steward <scala_steward@virtuslab.com>
2026-03-12 20:52:08 +0000

    Update fs2-core to 3.13.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -30,7 +30,7 @@ object Versions {
   val Sttp                    = "3.11.0"
   val SttpModel               = "1.7.17"
   val SttpShared              = "1.5.0"
-  val Fs2                     = "3.12.2"
+  val Fs2                     = "3.13.0"
   val ThreeTenExtra           = "1.8.0"
   val ScalaJavaTime           = "2.6.0"
 }
